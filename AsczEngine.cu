42a1096a66476c97f36551fae49a4b47e6a7edac
DrCornyKelp <hieuanh01122004@gmail.com>
2024-11-04 07:49:49 +0700

    updated

--- AsczEngine.cu
+++ AsczEngine.cu
@@ -302,7 +302,7 @@ int main() {
             }
 
             for (Cube3D &cube : cubes) cube.physic();
-            CAMERA.pos += CAMERA.vel;
+            CAMERA.pos += CAMERA.vel * .3;
         }
 
         // Press T to read an transform.txt file and apply it
